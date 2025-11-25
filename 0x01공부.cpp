60a2bcb75e0be9bd7f73fba4d1e58336ee7b8739
mollang03 <candy_carry1@naver.com>
2025-11-25 17:25:16 +0900

    공부하는중

---
+++ 0x01공부.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // 전처리 지시자
+
+/*
+c++에서 함수를 사용하자고 한다면..?
+반드시 그 함수의 원형을 미리 정의하여야 한다.
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
